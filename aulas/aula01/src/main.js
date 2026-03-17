9915b6258cb7240f3971d4b8dff9f5a7e047c05d
Kathia <kathiamfaria@gmail.com>
2026-03-17 10:56:14 -0300

    adiciona codigo javascript

--- aulas/aula01/src/main.js
+++ aulas/aula01/src/main.js
@@ -1,1 +1,39 @@
 import './style.css'
+/*
+validar o formulario com as regras:
+ - matricula > 0 e obrigatoria
+ - senha tamanho > 6 caracteres e obrigatorio
+ */
+
+const matricula = document.querySelector('#Matricula');
+const senha = document.querySelector('#Senha');
+const botao = document.querySelector('button');
+
+botao.addEventListener('click', function(e) {
+
+    const matriculaErro = document.querySelector("#matriculaErro");
+    const senhaErro = document.querySelector("#senhaErro");
+
+    matriculaErro.textContent = '';
+    senhaErro.textContent = '';
+
+if (matricula.value == ''){
+    matriculaErro.textContent = 'Matricula é obrigatoria';
+return;
+}
+
+if (parseInt(matricula.value) <= 0) {
+    matriculaErro.textContent = 'Matrícula deve ser um numero positivo';
+    return;
+}
+
+if (senha.value == ''){
+   senhaErro.textContent = 'Senha é obrigatoria';
+    return;
+}
+
+if (senha.value.length < 8){
+   senhaErro.textContent = 'Senha deve ter no mínimo 8 caracteres';
+    return;
+}
+});
