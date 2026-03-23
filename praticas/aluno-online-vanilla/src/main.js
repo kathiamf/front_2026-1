f694fefe2e78dda30d514fe4cf62e7cfd26e0596
Kathia <kathiamfaria@gmail.com>
2026-03-23 10:44:33 -0300

    feat: conclui pratica02. Fecha #3

--- praticas/aluno-online-vanilla/src/main.js
+++ praticas/aluno-online-vanilla/src/main.js
@@ -1,1 +1,35 @@
-import './style.css'
+import './login.css'
+
+const email = document.querySelector('#email')
+const senha = document.querySelector('#senha')
+const botao = document.querySelector('button')
+
+botao.addEventListener('click', function(e){
+
+    const emailErro = document.querySelector('#emailErro')
+    const senhaErro = document.querySelector('#senhaErro')
+
+    emailErro.textContent = '';
+    senhaErro.textContent = '';
+
+    let valido = true;
+
+    if (email.value == ''){
+        emailErro.textContent = 'O Campo do e-mail é obrigatório';
+        return;
+    }
+
+        if (senha.value == '') {
+        senhaErro.textContent = 'O Campo da senha é obrigatório';
+        return;
+    }
+
+    if (senha.value.length < 8) {
+        senhaErro.textContent = 'Senha deve ter no minímo 8 caracteres';
+        return;
+    }
+
+    if (valido) {
+        window.location.href = "index.html";
+    }
+})
