b4ab80fe9807aa00b947be9ad72b1698991767cd
kathiamf <kathiamfaria@gamil.com>
2026-06-15 09:13:28 -0300

    feat: conclui pratica09. Fecha #17

--- praticas/aluno-online-react/src/App.jsx
+++ praticas/aluno-online-react/src/App.jsx
@@ -1,4 +1,4 @@
-import { Routes, Route } from "react-router";
+import { Routes, Route, Navigate } from "react-router";
 import Dashboard from "./pages/Dashboard";
 import Faltas from "./pages/Faltas";
 import Notas from "./pages/Notas";
@@ -8,9 +8,10 @@ import Login from "./pages/Login";
 import Erro404 from "./pages/Erro404";
 import Layout from "./layout/Layout";
 import useAuth from "./hooks/useAuth";
+import RequerimentoForm from "./forms/RequerimentoForm";
 
 function App() {
-  const {logado} = useAuth();
+  const { logado } = useAuth();
 
   return (
     <Routes>
@@ -20,13 +21,19 @@ function App() {
           <Route path="notas" element={<Notas />} />
           <Route path="faltas" element={<Faltas />} />
           <Route path="boletos" element={<Boletos />} />
-          <Route path="requerimentos" element={<Requerimentos />} />
+          <Route path="requerimentos" element={<Requerimentos />}>
+            <Route path="novo" element={<RequerimentoForm />} />
+          </Route>
         </Route>
       ) : (
-        <Route path="/login" element={<Login />} />
+        <>
+          <Route path="/login" element={<Login />} />
+          <Route path="*" element={<Navigate to="/login" replace />} />
+        </>
       )}
-      <Route path="*" element={<Erro404 />} /> 
+      <Route path="*" element={<Erro404 />} />
     </Routes>
   );
 }
+
 export default App;
--- praticas/aluno-online-react/src/pages/Requerimentos.jsx
+++ praticas/aluno-online-react/src/pages/Requerimentos.jsx
@@ -1,45 +1,56 @@
-import { Outlet, useNavigate } from "react-router";
+import { useEffect, useState } from "react";
+import { Outlet, useNavigate, useLocation } from "react-router";
+import { listar } from "../services/requerimentosService";
 import Main from "../components/Main";
-import Sidebar from "../components/Sidebar";
 import Tabela from "../components/Tabela";
 
 function Requerimentos() {
-  const colunas = ["Tipo de Requerimento", "Data de Solicitação", "Situação"];
   const navigate = useNavigate();
+  const location = useLocation();
+  const [dados, setDados] = useState([]);
 
-  const informacoes = [
-    [
-      ["Revisão de Mensão", "15/12/2025", "Indeferido"],
-      ["Dispensa de Disciplina", "12/05/2025", "Indeferido"],
-      ["Trancamento de Matrícula", "05/01/2024", "Deferido"],
-      ["Mudança de Turno", "10/10/2023", "Deferido"],
-      ["Renovação de Matrícula", "20/02/2023", "Deferido"],
-      ["Mudança de Turno", "10/10/2023", "Em análise"],
-      ["Renovação de Matrícula", "20/02/2023", "Em análise"],
-      ["Mudança de Turno", "10/10/2023", "Em análise"],
-      ["Renovação de Matrícula", "20/02/2023", "Em análise"],
-    ],
-  ];
+  const colunas = ["Tipo de Requerimento", "Data de Solicitação", "Situação"];
+
+  const exibirTabela = location.pathname === "/requerimentos";
+
+  useEffect(() => {
+    if (exibirTabela) {
+      const carregar = async () => {
+        const resposta = await listar();
+        setDados(resposta);
+      };
+      carregar();
+    }
+  }, [location.pathname]);
 
   return (
-    <>
-      <Main
-        titulo="Meus Requerimentos"
-        subtitulo="Faça solitações online para Scretária"
-        className="flex-1 h-screen md:overflow-hidden"
-      >
-        <Tabela colunas={colunas} dados={informacoes[0]} />
+    <Main
+      titulo="Meus Requerimentos"
+      subtitulo="Faça solicitações online para Secretária"
+      className="flex-1 h-screen md:overflow-hidden"
+    >
+      {exibirTabela ? (
+        <>
+          <Tabela
+            colunas={colunas}
+            dados={dados.map((item) => [
+              item.tipo,
+              item.dataSolicitacao,
+              item.situacao,
+            ])}
+          />
 
-        <button
-          onClick={() => navigate("/novo")}
-          className="block ml-auto bg-gray-800 hover:bg-gray-700 active:bg-gray-900 text-white text-sm font-medium px-4 py-2 mr-4 rounded-lg transition-colors"
-        >
-          Novo Requerimento
-        </button>
-
+          <button
+            onClick={() => navigate("/requerimentos/novo")}
+            className="block ml-auto bg-gray-800 hover:bg-gray-700 active:bg-gray-900 text-white text-sm font-medium px-4 py-2 mr-4 rounded-lg transition-colors"
+          >
+            Novo Requerimento
+          </button>
+        </>
+      ) : (
         <Outlet />
-      </Main>
-    </>
+      )}
+    </Main>
   );
 }
 
